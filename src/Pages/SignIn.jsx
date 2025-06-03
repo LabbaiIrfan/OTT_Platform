2b9d87b642bd14237576f26ca8df523a3c898b00
LabbaiIrfan <labbaiirfan955@gmail.com>
2025-06-03 13:09:41 +0530

    remove reveal

--- src/Pages/SignIn.jsx
+++ src/Pages/SignIn.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useState, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { Fade } from "react-reveal";
 import { ClipLoader } from "react-spinners";
 import GlobixLogo from "../images/globix.png"
 import {
@@ -125,146 +124,144 @@ function SignIn() {
     >
       <div className="h-[100vh] flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <div className="w-full bg-[#000000a2] rounded-lg shadow sm:my-0 md:mt-0 sm:max-w-lg xl:p-0 border-2 border-stone-800 lg:border-0">
-          <Fade>
-            <div>
-              <div className="p-6 space-y-4 md:space-y-6 sm:p-12">
-                <div className="w-full flex justify-center mb-6">
-                  <img
-                    src={GlobixLogo}
-                    alt="Netflix Logo"
-                    className="h-16"
-                  />
+          <div>
+            <div className="p-6 space-y-4 md:space-y-6 sm:p-12">
+              <div className="w-full flex justify-center mb-6">
+                <img
+                  src={GlobixLogo}
+                  alt="Netflix Logo"
+                  className="h-16"
+                />
+              </div>
+              <h1 className="text-xl font-bold leading-tight tracking-tight text-white md:text-2xl dark:text-white">
+                Sign in to your account
+              </h1>
+              <form
+                onSubmit={handleSubmit}
+                className="space-y-4 md:space-y-6"
+                action="#"
+              >
+                <div>
+                  <label
+                    for="email"
+                    className="block mb-2 text-sm font-medium text-white dark:text-white"
+                  >
+                    Your email
+                  </label>
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    className={
+                      ErrorMessage
+                        ? "bg-stone-700 text-white sm:text-sm rounded-sm focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 border-2 border-red-700  dark:placeholder-white dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 placeholder:text-white"
+                        : "bg-stone-700 text-white sm:text-sm rounded-sm focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:placeholder-white dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 placeholder:text-white"
+                    }
+                    placeholder="name@email.com"
+                    required=""
+                    onChange={(e) => setEmail(e.target.value)}
+                  ></input>
                 </div>
-                <h1 className="text-xl font-bold leading-tight tracking-tight text-white md:text-2xl dark:text-white">
-                  Sign in to your account
-                </h1>
-                <form
-                  onSubmit={handleSubmit}
-                  className="space-y-4 md:space-y-6"
-                  action="#"
-                >
-                  <div>
-                    <label
-                      for="email"
-                      className="block mb-2 text-sm font-medium text-white dark:text-white"
-                    >
-                      Your email
-                    </label>
-                    <input
-                      type="email"
-                      name="email"
-                      id="email"
-                      className={
-                        ErrorMessage
-                          ? "bg-stone-700 text-white sm:text-sm rounded-sm focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 border-2 border-red-700  dark:placeholder-white dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 placeholder:text-white"
-                          : "bg-stone-700 text-white sm:text-sm rounded-sm focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:placeholder-white dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 placeholder:text-white"
-                      }
-                      placeholder="name@email.com"
-                      required=""
-                      onChange={(e) => setEmail(e.target.value)}
-                    ></input>
-                  </div>
-                  <div>
-                    <label
-                      for="password"
-                      className="block mb-2 text-sm font-medium text-white dark:text-white"
-                    >
-                      Password
-                    </label>
-                    <input
-                      type="password"
-                      name="password"
-                      id="password"
-                      placeholder="••••••••"
-                      className={
-                        ErrorMessage
-                          ? "bg-stone-700 text-white sm:text-sm rounded-sm focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  border-2 border-red-700 dark:bg-gray-700 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 placeholder:text-white"
-                          : "bg-stone-700 text-white sm:text-sm rounded-sm focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 placeholder:text-white"
-                      }
-                      required=""
-                      onChange={(e) => setPassword(e.target.value)}
-                    ></input>
-                  </div>
-                  <div>
-                    {ErrorMessage && (
-                      <h1 className="flex text-white font-bold p-4 bg-red-700 rounded text-center">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth={1.5}
-                          stroke="currentColor"
-                          className="w-6 h-6 mr-1"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z"
-                          />
-                        </svg>
-                        {ErrorMessage}
-                      </h1>
-                    )}
-                  </div>
-                  <div className="flex items-center justify-between">
-                    <div className="flex items-start">
-                      <div className="flex items-center h-5">
-                        <input
-                          id="remember"
-                          aria-describedby="remember"
-                          type="checkbox"
-                          className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                          required=""
-                        ></input>
-                      </div>
-                      <div className="ml-3 text-sm">
-                        <label
-                          for="remember"
-                          className="text-gray-500 dark:text-gray-300"
-                        >
-                          Remember me
-                        </label>
-                      </div>
+                <div>
+                  <label
+                    for="password"
+                    className="block mb-2 text-sm font-medium text-white dark:text-white"
+                  >
+                    Password
+                  </label>
+                  <input
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="••••••••"
+                    className={
+                      ErrorMessage
+                        ? "bg-stone-700 text-white sm:text-sm rounded-sm focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5  border-2 border-red-700 dark:bg-gray-700 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 placeholder:text-white"
+                        : "bg-stone-700 text-white sm:text-sm rounded-sm focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 placeholder:text-white"
+                    }
+                    required=""
+                    onChange={(e) => setPassword(e.target.value)}
+                  ></input>
+                </div>
+                <div>
+                  {ErrorMessage && (
+                    <h1 className="flex text-white font-bold p-4 bg-red-700 rounded text-center">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-6 h-6 mr-1"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z"
+                        />
+                      </svg>
+                      {ErrorMessage}
+                    </h1>
+                  )}
+                </div>
+                <div className="flex items-center justify-between">
+                  <div className="flex items-start">
+                    <div className="flex items-center h-5">
+                      <input
+                        id="remember"
+                        aria-describedby="remember"
+                        type="checkbox"
+                        className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                        required=""
+                      ></input>
+                    </div>
+                    <div className="ml-3 text-sm">
+                      <label
+                        for="remember"
+                        className="text-gray-500 dark:text-gray-300"
+                      >
+                        Remember me
+                      </label>
                     </div>
                   </div>
-                  <button
-                    type="submit"
-                    className={`w-full text-white ${loader
-                        ? `bg-stone-700`
-                        : `bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-primary-300`
-                      } transition ease-in-out font-medium rounded-sm text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800`}
+                </div>
+                <button
+                  type="submit"
+                  className={`w-full text-white ${loader
+                      ? `bg-stone-700`
+                      : `bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-primary-300`
+                    } transition ease-in-out font-medium rounded-sm text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800`}
+                >
+                  {loader ? <ClipLoader color="#ff0000" /> : `Sign in`}
+                </button>
+                <button
+                  onClick={loginWithGoogle}
+                  className={`flex justify-center items-center w-full text-white ${loader
+                      ? `bg-stone-700`
+                      : `bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-primary-300`
+                    } transition ease-in-out font-medium rounded-sm text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:focus:ring-primary-800`}
+                >
+                  {loader ? (
+                    <ClipLoader color="#ff0000" />
+                  ) : (
+                    <>
+                      <img className="w-8" src={GoogleLogo}></img>{" "}
+                      <p className="ml-1">Sign in with Google</p>
+                    </>
+                  )}
+                </button>
+                <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                  Don't have an account yet?{" "}
+                  <Link
+                    className="font-medium text-white hover:underline dark:text-primary-500"
+                    to={"/signup"}
                   >
-                    {loader ? <ClipLoader color="#ff0000" /> : `Sign in`}
-                  </button>
-                  <button
-                    onClick={loginWithGoogle}
-                    className={`flex justify-center items-center w-full text-white ${loader
-                        ? `bg-stone-700`
-                        : `bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-primary-300`
-                      } transition ease-in-out font-medium rounded-sm text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:focus:ring-primary-800`}
-                  >
-                    {loader ? (
-                      <ClipLoader color="#ff0000" />
-                    ) : (
-                      <>
-                        <img className="w-8" src={GoogleLogo}></img>{" "}
-                        <p className="ml-1">Sign in with Google</p>
-                      </>
-                    )}
-                  </button>
-                  <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                    Don’t have an account yet?{" "}
-                    <Link
-                      className="font-medium text-white hover:underline dark:text-primary-500"
-                      to={"/signup"}
-                    >
-                      Sign up
-                    </Link>
-                  </p>
-                </form>
-              </div>
+                    Sign up
+                  </Link>
+                </p>
+              </form>
             </div>
-          </Fade>
+          </div>
         </div>
       </div>
     </section>
--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -3,7 +3,6 @@ import { useState, useContext } from "react";
 import GlobixLogo from "../images/globix.png"
 
 import { Link, useNavigate } from "react-router-dom";
-import { Fade } from "react-reveal";
 import {
   getAuth,
   createUserWithEmailAndPassword,
@@ -88,128 +87,126 @@ function SignUp() {
     >
       <div className="h-[100vh] flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <div className="w-full bg-[#000000a2] rounded-lg shadow sm:my-0 md:mt-0 sm:max-w-lg xl:p-0 border-2 border-stone-800 lg:border-0">
-          <Fade>
-            <div>
-              <div className="p-6 space-y-4 md:space-y-6 sm:p-12">
-                <div className="w-full flex justify-center mb-6">
-                  <img
-                    src={GlobixLogo}
-                    alt="Netflix Logo"
-                    className="h-16"
-                  />
+          <div>
+            <div className="p-6 space-y-4 md:space-y-6 sm:p-12">
+              <div className="w-full flex justify-center mb-6">
+                <img
+                  src={GlobixLogo}
+                  alt="Netflix Logo"
+                  className="h-16"
+                />
+              </div>
+              <h1 className="text-xl font-bold leading-tight tracking-tight text-white md:text-2xl dark:text-white">
+                Create a new account
+              </h1>
+              <form
+                onSubmit={handleSubmit}
+                className="space-y-4 md:space-y-6"
+                action="#"
+              >
+                <div>
+                  <label
+                    for="email"
+                    className="block mb-2 text-sm font-medium text-white dark:text-white"
+                  >
+                    Your email
+                  </label>
+                  <input
+                    onChange={(e) => setEmail(e.target.value)}
+                    type="email"
+                    name="email"
+                    id="email"
+                    className={
+                      ErrorMessage
+                        ? "bg-stone-700 text-white sm:text-sm rounded-sm border-2 border-red-700 focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:text-white "
+                        : "bg-stone-700 text-white sm:text-sm rounded-sm focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:text-white "
+                    }
+                    placeholder="name@emil.com"
+                    required=""
+                  ></input>
                 </div>
-                <h1 className="text-xl font-bold leading-tight tracking-tight text-white md:text-2xl dark:text-white">
-                  Create a new account
-                </h1>
-                <form
-                  onSubmit={handleSubmit}
-                  className="space-y-4 md:space-y-6"
-                  action="#"
-                >
-                  <div>
-                    <label
-                      for="email"
-                      className="block mb-2 text-sm font-medium text-white dark:text-white"
-                    >
-                      Your email
-                    </label>
-                    <input
-                      onChange={(e) => setEmail(e.target.value)}
-                      type="email"
-                      name="email"
-                      id="email"
-                      className={
-                        ErrorMessage
-                          ? "bg-stone-700 text-white sm:text-sm rounded-sm border-2 border-red-700 focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:text-white "
-                          : "bg-stone-700 text-white sm:text-sm rounded-sm focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:text-white "
-                      }
-                      placeholder="name@emil.com"
-                      required=""
-                    ></input>
-                  </div>
-                  <div>
-                    <label
-                      for="password"
-                      className="block mb-2 text-sm font-medium text-white dark:text-white"
-                    >
-                      Password
-                    </label>
-                    <input
-                      onChange={(e) => setPassword(e.target.value)}
-                      type="password"
-                      name="password"
-                      id="password"
-                      placeholder="••••••••"
-                      className={
-                        ErrorMessage
-                          ? "bg-stone-700 text-white sm:text-sm rounded-sm border-2 border-red-700 focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
-                          : "bg-stone-700 text-white sm:text-sm rounded-sm focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:text-white"
-                      }
-                      required=""
-                    ></input>
-                  </div>
-                  <div>
-                    {ErrorMessage && (
-                      <h1 className="flex text-white font-bold p-4 bg-red-700 rounded text-center">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth={1.5}
-                          stroke="currentColor"
-                          className="w-6 h-6 mr-1"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z"
-                          />
-                        </svg>
+                <div>
+                  <label
+                    for="password"
+                    className="block mb-2 text-sm font-medium text-white dark:text-white"
+                  >
+                    Password
+                  </label>
+                  <input
+                    onChange={(e) => setPassword(e.target.value)}
+                    type="password"
+                    name="password"
+                    id="password"
+                    placeholder="••••••••"
+                    className={
+                      ErrorMessage
+                        ? "bg-stone-700 text-white sm:text-sm rounded-sm border-2 border-red-700 focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5"
+                        : "bg-stone-700 text-white sm:text-sm rounded-sm focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:text-white"
+                    }
+                    required=""
+                  ></input>
+                </div>
+                <div>
+                  {ErrorMessage && (
+                    <h1 className="flex text-white font-bold p-4 bg-red-700 rounded text-center">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-6 h-6 mr-1"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z"
+                        />
+                      </svg>
 
-                        {ErrorMessage}
-                      </h1>
-                    )}
-                  </div>
-                  <div className="flex items-center justify-between">
-                    <div className="flex items-start">
-                      <div className="flex items-center h-5">
-                        <input
-                          id="remember"
-                          aria-describedby="remember"
-                          type="checkbox"
-                          className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 "
-                          required=""
-                        ></input>
-                      </div>
-                      <div className="ml-3 text-sm">
-                        <label for="remember" className="text-gray-500">
-                          Remember me
-                        </label>
-                      </div>
+                      {ErrorMessage}
+                    </h1>
+                  )}
+                </div>
+                <div className="flex items-center justify-between">
+                  <div className="flex items-start">
+                    <div className="flex items-center h-5">
+                      <input
+                        id="remember"
+                        aria-describedby="remember"
+                        type="checkbox"
+                        className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 "
+                        required=""
+                      ></input>
+                    </div>
+                    <div className="ml-3 text-sm">
+                      <label for="remember" className="text-gray-500">
+                        Remember me
+                      </label>
                     </div>
                   </div>
-                  <button
-                    type="submit"
-                    className={`w-full text-white ${loader
-                        ? `bg-stone-700`
-                        : `bg-red-800 focus:ring-4 focus:outline-none focus:ring-primary-300`
-                      } font-medium rounded-sm text-sm px-5 py-2.5 text-center`}
+                </div>
+                <button
+                  type="submit"
+                  className={`w-full text-white ${loader
+                      ? `bg-stone-700`
+                      : `bg-red-800 focus:ring-4 focus:outline-none focus:ring-primary-300`
+                    } font-medium rounded-sm text-sm px-5 py-2.5 text-center`}
+                >
+                  {loader ? <ClipLoader color="#ff0000" /> : "Create now"}
+                </button>
+                <p className="text-sm font-light text-gray-500">
+                  Already have one?{" "}
+                  <Link
+                    className="font-medium text-white hover:underline"
+                    to={"/signin"}
                   >
-                    {loader ? <ClipLoader color="#ff0000" /> : "Create now"}
-                  </button>
-                  <p className="text-sm font-light text-gray-500">
-                    Already have one?{" "}
-                    <Link
-                      className="font-medium text-white hover:underline"
-                      to={"/signin"}
-                    >
-                      Sign in
-                    </Link>
-                  </p>
-                </form>
-              </div>
+                    Sign in
+                  </Link>
+                </p>
+              </form>
             </div>
-          </Fade>
+          </div>
         </div>
       </div>
     </section>
